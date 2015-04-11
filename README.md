9203bf3af9d40620d17a8ef4f4204cdc1782e131
Swapnull <martyn.e.rushton@gmail.com>
2015-04-11 23:09:13 +0100

    Changed from using blog-builder to using metalsmith blog engine due to code stability and module flexibility.

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ This is a platform for me to write my thoughts and ideas down. The main aim is t
 ### Styling
 - The styling is currently lacking. This site needs to be styled with a similar feel to the root site (martynrushton.com) in mind. Keep it simple.
 - I would like to give the archive page either a similar tiled effect that I have been using on other sites, or look into other ways of styling.
-- I would like to make the home page (blog.martynrushton.com/) display the full posts of the last 10 posts. 
+- I would like to make the home page (blog.martynrushton.com/) display the full posts of the last 10 posts.
 ### URL rewrite
 Currently, when following the tag link you get a '/tags/<tag>.html', I would like to remove the .html. The internet says that this can be done with a
 .htaccess file. This is server side technology and I would like to look into it.
+### Comments
+Look into DISQUS for a comment system.
--- server.js
+++ server.js
@@ -1,23 +1,68 @@
 var http = require('http')
+var fs = require('fs')
 var ecstatic = require('ecstatic')
-var blogBuilder = require('blog-builder')
+
+var Metalsmith = require('metalsmith')
+var drafts = require('metalsmith-drafts');
+var markdown = require('metalsmith-markdown');
+var permalinks = require('metalsmith-permalinks');
+var templates = require('metalsmith-templates');
 
 
 /* Blog options */
-var options = require('./blog-config.js')
-var blog = new blogBuilder(options)
+var metalsmith = Metalsmith(__dirname)
+  .use(drafts())
+  .use(markdown())
+  .use(permalinks('posts/:title'))
+  .use(templates('handlebars'))
+  .destination('./build')
+  .build(function(err, files) {
+          if (err) {
+            console.log(err)
+            throw err;
+          } else{
+            var source = fs.createReadStream('./build/posts/home-page/index.html');
+            var dest = fs.createWriteStream('./build/index.html');
 
-blog.build()
-console.log('blog built')
+            source.pipe(dest);
+            source.on('end', function() {
+              console.log('index copied successfully');
+              deleteFolderRecursive('./build/posts/home-page')
+            });
+          source.on('error', function(err) { console.log('index copy failed')});
+          }
+      });
+
+//Delete folders at runtime so they are not accessible from the browser
+var deleteFolderRecursive = function(path) {
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath, function(err){
+          if(err){
+            throw err
+          }
+        });
+      }
+    });
+    fs.rmdirSync(path);
+  }
+  console.log('Removed folder ' + path)
+};
+
 
 
 /* Server setup */
 var staticd = ecstatic({
-  root: __dirname + '/public',
+  root: __dirname + '/build',
   showDir: true,
   autoIndex:true,
   gzip: true
 });
+
 
 var server = http.createServer(staticd)
 
